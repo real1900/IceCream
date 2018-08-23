d500a724683bb5331dc5d4baa054d4f69ea01e50
Yue Cai <yuecai.nju@gmail.com>
2018-08-23 09:18:24 +0800

    add macOS new target

---
+++ IceCream macOS/IceCream_macOS.h
@@ -0,0 +1,19 @@
+//
+//  IceCream_macOS.h
+//  IceCream macOS
+//
+//  Created by 蔡越 on 2018/8/22.
+//  Copyright © 2018 蔡越. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for IceCream_macOS.
+FOUNDATION_EXPORT double IceCream_macOSVersionNumber;
+
+//! Project version string for IceCream_macOS.
+FOUNDATION_EXPORT const unsigned char IceCream_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IceCream_macOS/PublicHeader.h>
+
+
